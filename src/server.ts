13eacadea2847b422d40d09acc268fedc5977402
Zo <70714420+LorenzoBalderrama@users.noreply.github.com>
2023-11-26 21:10:52 -0800

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import router from './router';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log('hello from express')
+    res.status(200)
+    res.json({message: 'hello'})
+})
+
+app.use('/api', router);
+
+export default app;
